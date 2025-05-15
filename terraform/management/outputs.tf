812453038aa085e2d672bc79bef2e52690d14043
Bradley Neumaier <neumaierbradley@icloud.com>
2025-05-15 15:34:38 -0400

    labs

--- terraform/management/outputs.tf
+++ terraform/management/outputs.tf
@@ -2,3 +2,7 @@ output "cloudtrail_arn" {
   description = "The ARN of the CloudTrail"
   value       = aws_cloudtrail.audit.arn
 }
+output "organization_id" {
+  description = "The ID of the AWS Organization"
+  value       = aws_organizations_organization.org.id
+}
